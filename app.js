ce1a2d5cc44aa5a6b91533c9f207238831d44e46
Kay‘s MacPro <kaysmacpro@Kaysdebijibendiannao.local>
2023-11-29 23:43:11 -0800

    a6

--- app.js
+++ app.js
@@ -5,15 +5,44 @@ import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
 import "dotenv/config";
-// const express = require('express')
+import UserRoutes from './users/routes.js';
+import mongoose from "mongoose";
+import session from "express-session";
+// mongoose.connect("mongodb://127.0.0.1:27017/kanbas-cs5610-fa23");
+
 
 const app = express()
-app.use(cors());
+app.use(cors(
+    {
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+      }     
+));
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
+
 app.use(express.json());
+
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
-Lab5(app);
+Lab5(app); 
 Hello(app);
 
 
-app.listen(process.env.PORT || 4000);
+app.listen(4000);
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
