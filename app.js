eb714bae56e0aedb2d6be16d5363df539b521dbb
Kay‘s MacPro <kaysmacpro@Kaysdebijibendiannao.local>
2023-11-30 10:59:07 -0800

    cors

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ const app = express()
 app.use(cors(
     {
         credentials: true,
-        origin: process.env.FRONTEND_URL
+        origin: '*',
+        // origin: process.env.FRONTEND_URL
       }     
 ));
 const sessionOptions = {
